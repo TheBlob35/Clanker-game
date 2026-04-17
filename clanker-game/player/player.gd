e782c2b63ba9f92fcf6a275f578a0b9e4d85d5a5
Karl Värna <25kava@ad.ssis.nu>
2026-04-17 11:52:12 +0200

    Boss scene and camera tweaks

--- clanker-game/player/player.gd
+++ clanker-game/player/player.gd
@@ -1,13 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 50
-#const DASH_SPEED = 500.0
-#const DASH_TIME = 0.12
-#const DASH_DELAY = 1.5
+const SPEED = 100
 
-#var dash_time_left := 0.0
-#var dash_cooldown := 0.0
-#var dash_direction := Vector2.ZERO
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
@@ -23,18 +17,6 @@ func _physics_process(delta):
 
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
-
-	#dash_time_left -= delta
-	#dash_cooldown -= delta
-
-	#if Input.is_action_just_pressed("ui_accept") and dash_cooldown <= 0 and direction != Vector2.ZERO:
-		#dash_time_left = DASH_TIME
-		#dash_cooldown = DASH_DELAY
-		#dash_direction = direction
-
-	#if dash_time_left > 0:
-		#velocity = dash_direction * DASH_SPEED
-	#else:
 	
 	velocity = direction * SPEED
 
@@ -46,10 +28,10 @@ var current_hp = 100
 
 func take_damage(amount):
 	current_hp -= amount
-	print("HP: ", current_hp)  # optional, useful for testing
+	print("HP: ", current_hp)
 	
 	if current_hp <= 0:
 		die()
 
 func die():
-	get_tree().quit()  # or load a game over screen, respawn, etc.
+	get_tree().quit()
